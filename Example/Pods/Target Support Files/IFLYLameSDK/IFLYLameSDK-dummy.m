0d80de90f917baf5dfac0d9e72e979dd7772978b
zhangchengcai <zhangchengcai3615@126.com>
2020-12-15 14:22:31 +0800

    本次修改一下内容：1234

--- Example/Pods/Target Support Files/IFLYLameSDK/IFLYLameSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IFLYLameSDK : NSObject
-@end
-@implementation PodsDummy_IFLYLameSDK
-@end
--- Example/Pods/Target Support Files/Pods-IFLYLameSDK_Tests/Pods-IFLYLameSDK_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IFLYLameSDK_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_IFLYLameSDK_Tests
-@end
